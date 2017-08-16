dc72d280b2e18d7d682e9bdf4ec8aeb65d49d599
Akiicat <aaaa1379@gmail.com>
2017-08-17 02:51:10 +0800

    A Carousel on Home Page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,57 @@
 <template>
-  <div>
-    <p>The Home Page</p>
-  </div>
+  <v-container>
+
+    <v-layout row wrap>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/meetups/new" class="info">Organize Meetup</v-btn>
+      </v-flex>
+    </v-layout>
+
+    <v-layout row wrap class="mt-2">
+      <v-carousel>
+        <v-carousel-item
+          v-for="meetup in meetups"
+          :src="meetup.imageUrl"
+          :key="meetup.id">
+          <div class="title">
+            {{ meetup.title }}
+          </div>
+        </v-carousel-item>
+      </v-carousel>
+    </v-layout>
+
+    <v-layout row wrap class="mb-2">
+      <v-flex xs12 class="text-xs-center">
+        <p>Join out awesome meetupds!</p>
+      </v-flex>
+    </v-layout>
+
+  </v-container>
 </template>
 
+<script>
+  export default {
+    data () {
+      return {
+        meetups: [
+          { imageUrl: 'https://i.imgur.com/rO883eh.jpg', id: 'saf', title: 'Meetup in Taipei' },
+          { imageUrl: 'https://i.imgur.com/0IGt7hM.jpg', id: 'saf', title: 'Meetup in Kauhsiung' }
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+.title {
+  position: absolute;
+  bottom: 50px;
+  background-color: rgba(0, 0, 0, .5);
+  color: white;
+  font-size: 2em;
+  padding: 20px;
+}
+</style>
