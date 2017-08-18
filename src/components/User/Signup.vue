b81bcdb7d7269e1d116165cd813b1f12b5498dfb
Akiicat <aaaa1379@gmail.com>
2017-08-18 15:33:05 +0800

    Auth State Feedback with Vuex

--- src/components/User/Signup.vue
+++ src/components/User/Signup.vue
@@ -1,5 +1,11 @@
 <template>
   <v-container>
+    <v-layout row v-if="error">
+      <v-flex xs12 sm6 offset-sm3>
+        <app-alert @dismissed="onDissmissed" :text="error.message"></app-alert>
+      </v-flex>
+    </v-layout>
+
     <v-layout row>
       <v-flex xs12 sm6 offset-sm3>
         <v-card>
@@ -44,7 +50,12 @@
 
               <v-layout row>
                 <v-flex xs12>
-                  <v-btn type="submit">Sign up</v-btn>
+                  <v-btn type="submit" :disabled="loading" :loading="loading">
+                    Sign up
+                    <span slot="loader" class="custom-loader">
+                      <v-icon light>cached</v-icon>
+                    </span>
+                  </v-btn>
                 </v-flex>
               </v-layout>
             </form>
@@ -70,6 +81,12 @@ export default {
     },
     user () {
       return this.$store.getters.user
+    },
+    error () {
+      return this.$store.getters.error
+    },
+    loading () {
+      return this.$store.getters.loading
     }
   },
   watch: {
@@ -85,7 +102,11 @@ export default {
         email: this.email,
         password: this.password
       })
+    },
+    onDissmissed () {
+      this.$store.dispatch('clearError')
     }
   }
 }
 </script>
+
